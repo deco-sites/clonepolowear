031c5290b0db8c5fd8ba7d39a50188b7ebf876b9
everson <eversond14s@gmail.com>
2023-05-21 02:45:23 -0300

    create informative

--- sections/containerInformative/ContainerInformative.tsx
+++ sections/containerInformative/ContainerInformative.tsx
@@ -3,26 +3,29 @@ export interface Props {
     url: string;
     alt: string;
     content: string;
-  }>,
-  sizeIcon?: number
+  }>;
+  sizeIcon?: number;
 }
 
-function ContainerInformative({informative, sizeIcon=30}: Props) {
-  return(
+function ContainerInformative({ informative, sizeIcon = 30 }: Props) {
+  return (
     <div className="flex justify-evenly bg-slate-300 h-28">
-      {
-        informative.map(({url, content, alt}) => (
-          <div
-            style="display:flex;flex-direction:column;justify-content: center;"
-            className="w-[125px] text-center"
-          >
-            <img className="m-auto my-0" width={`${sizeIcon}px`} src={url} alt={alt}/>
-            <p className="text-xs font-bold">{content}</p>
-          </div>
-        ))
-      }
+      {informative.map(({ url, content, alt }) => (
+        <div
+          style="display:flex;flex-direction:column;justify-content: center;"
+          className="w-[125px] text-center"
+        >
+          <img
+            className="m-auto my-0"
+            width={`${sizeIcon}px`}
+            src={url}
+            alt={alt}
+          />
+          <p className="text-xs font-bold">{content}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default ContainerInformative;
